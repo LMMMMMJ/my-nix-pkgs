3058fde0c6d18b8cf674457b125d8ed7ad1304fe
LMMMMMJ <swufe_jacobliu@163.com>
2025-10-17 11:11:49 +0800

    Update dependencies for claude-code, claude-code-router, and gemini-cli; bump versions and hashes in respective Nix files.

--- pkgs/claude-code/default.nix
+++ pkgs/claude-code/default.nix
@@ -7,16 +7,16 @@
 
 buildNpmPackage rec {
   pname = "claude-code";
-  version = "1.0.126";
+  version = "2.0.21";
 
   nodejs = nodejs_20; # required for sandboxed Nix builds on Darwin
 
   src = fetchzip {
     url = "https://registry.npmjs.org/@anthropic-ai/claude-code/-/claude-code-${version}.tgz";
-    hash = "sha256-U6uYRkmVqMoJDAAzLHpF9G5OglPhLqPuwe6gWMQPx78=";
+    hash = "sha256-sX9btcy9uEHloAQNvCJFhwh0U/W14NWz2FjkdLXm1Q0=";
   };
 
-  npmDepsHash = "sha256-0aEyLR+4Yl5IivhEcdpnr2/0YlWxHxtB8r7sWceC3WY=";
+  npmDepsHash = "sha256-5wNDvsz61CMntrq/u25/DzSqHC+ofClF9rz/OXNLJho=";
 
   postPatch = ''
     cp ${./package-lock.json} package-lock.json
