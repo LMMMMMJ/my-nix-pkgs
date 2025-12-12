c6e9c280b1ad3c8fcc301b6cc5a6a2d48f5ccdc1
LMMMMMJ <swufe_jacobliu@163.com>
2025-12-12 10:55:51 +0800

    Update flake.nix and flake.lock to reference nixpkgs version 25.11, reflecting changes in lastModified timestamp, narHash, and revision.

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Provide extra Nix packages for my custom modules.";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     codex-nix.url = "github:sadjow/codex-nix";
     utils.url = "github:numtide/flake-utils";
   };
